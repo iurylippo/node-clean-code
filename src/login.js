54564b5455ccd467914b33b069bf98dd31da3d8a
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-10 01:58:39 -0300

    feat: init clean arch

---
+++ src/login.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+
+module.exports = () => {
+  const router = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(router))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+// presentation
+// signup-router
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
+
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+// domain
+// signup-use-case
+const AccountModel = mongoose.model('Account')
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      return new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// infra
+// account-repository
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
+}
