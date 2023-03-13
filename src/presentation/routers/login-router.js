2af5aa26c437b0b64486161922a80f94fe89583d
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-13 19:03:50 -0300

    feat: change HttpResponse badrequest to accept custom error

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,4 +1,5 @@
 import { HttpResponse } from '../helpers/http-response'
+import { MissingParamError } from '../helpers/missing-param-error'
 
 export class LoginRouter {
   constructor (authUserCase) {
@@ -10,11 +11,11 @@ export class LoginRouter {
       const { email, password } = httpRequest.body
 
       if (!email) {
-        return HttpResponse.badRequest('email')
+        return HttpResponse.badRequest(new MissingParamError('email'))
       }
 
       if (!password) {
-        return HttpResponse.badRequest('password')
+        return HttpResponse.badRequest(new MissingParamError('password'))
       }
 
       const accessToken = await this.authUserCase.auth(email, password)
