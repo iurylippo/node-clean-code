501bc6055a0bc613a87ea3ded664329707404ec6
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-14 22:47:53 -0300

    mock email validator return result

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,15 +1,30 @@
+import validator from 'validator'
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
+  const emailValidatorMock = jest.spyOn(validator, 'isEmail')
+
   it('Should return true if validator returns true', () => {
     const sut = new EmailValidator()
+
+    emailValidatorMock.mockImplementation(() => true)
 
     const isEmailValid = sut.isValid('valid_email@email.com')
 
     expect(isEmailValid).toBe(true)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    emailValidatorMock.mockImplementation(() => false)
+
+    const isEmailValid = sut.isValid('email_not_valid')
+
+    expect(isEmailValid).toBe(false)
   })
 })
