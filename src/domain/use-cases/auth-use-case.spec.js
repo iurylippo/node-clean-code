25055becc8fd58392d589c950b47bbab8d544543
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-15 23:41:49 -0300

    create makeSut helper and move sut

--- src/domain/use-cases/auth-use-case.spec.js
+++ src/domain/use-cases/auth-use-case.spec.js
@@ -18,29 +18,38 @@ class AuthUseCase {
   }
 }
 
+const makeSut = () => {
+  class LoadUserByEmailRepositorySpy {
+    load = jest.fn(async () => null)
+  }
+
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+
+  return {
+    sut,
+    loadUserByEmailRepositorySpy
+  }
+}
+
 describe('Auth UseCase', () => {
   it('Should throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
 
     expect(promise).rejects.toThrowError(new MissingParamError('email'))
   })
 
   it('Should throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@email')
 
     expect(promise).rejects.toThrowError(new MissingParamError('password'))
   })
 
   it('Should call loadUserByEmailRepository with correct email', async () => {
-    class LoadUserByEmailRepositorySpy {
-      load = jest.fn(async () => null)
-    }
-
-    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-
-    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
     const email = 'any_email@email'
     await sut.auth(email, 'pass')
 
