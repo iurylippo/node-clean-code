53cf8f43d0a9740eac95c01b0c3a9e2d7f7b24d4
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2023-03-15 23:39:36 -0300

    ensure load user repo to be called with the correct email

--- src/domain/use-cases/auth-use-case.spec.js
+++ src/domain/use-cases/auth-use-case.spec.js
@@ -1,6 +1,10 @@
 import { MissingParamError } from '../../utils/errors/missing-param-error'
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -9,6 +13,8 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
@@ -25,5 +31,19 @@ describe('Auth UseCase', () => {
     const promise = sut.auth('any_email@email')
 
     expect(promise).rejects.toThrowError(new MissingParamError('password'))
+  })
+
+  it('Should call loadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      load = jest.fn(async () => null)
+    }
+
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+    const email = 'any_email@email'
+    await sut.auth(email, 'pass')
+
+    expect(loadUserByEmailRepositorySpy.load).toBeCalledWith(email)
   })
 })
